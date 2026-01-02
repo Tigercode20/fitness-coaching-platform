3836cfa3fb291958ce3c6d34896882b33fe0cd15
Tigercode20 <mohamedwork222@gmail.com>
2026-01-02 18:27:51 +0200

    Configure Vite alias for Parse to use browser build and fix Node.js dependency errors

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+            parse: 'parse/dist/parse.min.js',
         },
     },
     server: {
